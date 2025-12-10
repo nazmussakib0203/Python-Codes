3b79f61b52598c7dbbb0d6020f05599df56d794d
Nazmus Sakib <sakibn916@gmail.com>
2025-12-10 13:34:44 +0600

    python codes

---
+++ test.py
@@ -0,0 +1,5 @@
+name=input("Enter your name:")
+age=input("Enter your age:")
+goal=input("Enter your learning goal:")
+print("Hi,i am ",name,".","I am ",age,"years old and my learning goal is to ",goal)
+
